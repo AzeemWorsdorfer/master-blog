3c6685fa67c6d24209ba356c92a88ec9afb56bfc
AzeemWorsdorfer <mohazeem327@gmail.com>
2025-11-14 10:56:31 +0100

    Added Basic storage system JSON
    Created static and templates files with index.html and styles.css
    
    Added load logic to read blog get blog posts from JSON file

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+import json
+from flask import Flask, render_template
+
+BLOG_POST_FILE = "blog_posts.json"
+
+
+def load_posts():
+    """ Loads blog posts from JSON file"""
+    try:
+        with open(BLOG_POST_FILE, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print(f"ERROR: The file {BLOG_POST_FILE} was not found.")
+        return []
+    except json.JSONDecodeError:
+        print(
+            f"ERROR: Could not decode JSON from {BLOG_POST_FILE}. Check file format.")
+        return []
+
+
+blog_posts = load_posts()
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello, World!"
+    return render_template("index.html", posts=blog_posts)
 
 
 if __name__ == "__main__":
